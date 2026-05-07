a3d7ecb387e487bf820c6e31cd34e837f8c2acf4
Satendra kushwaha <satendrakushwaha1030@gmail.com>
2026-05-07 19:30:03 +0545

    Fix API connection

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,9 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import { GoogleGenerativeAI } from "@google/generative-ai";
 dotenv.config();
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
